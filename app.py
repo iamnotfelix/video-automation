4cfd27083d56a7dc07b2c22eb3a4e927bf9fc4ea
iamnotfelix <andrei.iliescu03@gmail.com>
2023-07-13 23:24:11 +0300

    Improved main script.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from instagram_scrapper import InstagramScrapper
 from video_builder_ffmpeg import VideoBuilderFfmpeg
 from video_builder_moviepy import VideoBuilderMoviePy
+from utils import timeit
 
 
 DURATION = 3 * 60               # the length of the result videos in seconds (i.e. 10 * 60 = 600 = 10 minutes)
@@ -9,22 +10,30 @@ SOURCE_PATH = "sources.txt"     # path to file containing instagram usernames to
 TARGET_PATH = "./results"       # path to folder where the final videos will be
 VIDEOS_PATH = "./archive"       # path to folder where the scrapped videos will end up
 USE_FFMPEG = True               # change to False if you want to use moviepy(slower than ffmpeg)
-RESULT_NAME = "out.mp4"         # name of the resulted video
-
-insta = InstagramScrapper()
-
-# Open text file containing pages to scrape
-with open(SOURCE_PATH, "r") as f:
-    lines = f.readlines()
-    for line in lines:
-        user = line.strip("\n ")
-        videos = insta.get_videos_from_interval(user, datetime(2023, 7, 10), datetime.now())
-        insta.download_videos(videos, "archive", user)
+RESULT_NAME = "out3.mp4"        # name of the resulted video
 
 
-if USE_FFMPEG:
+@timeit("Scrapping videos took ")
+def scrapping():
+    print("Started scrapping...")
+    insta = InstagramScrapper()
+
+    # Open text file containing pages to scrape
+    with open(SOURCE_PATH, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            user = line.strip("\n ")
+            @timeit(f"Scrapping {line}")
+            def tmp():
+                videos = insta.get_videos_from_interval(user, datetime(2023, 7, 12), datetime.now())
+                insta.download_videos(videos, "archive", user)
+            tmp()
+
+@timeit("Building video with ffmpeg took ")
+def building_video_ffmpeg():
+    print("Started building video...")
     builder = VideoBuilderFfmpeg()
-
+    
     builder.concat_videos_from_folder(
         folder_path=VIDEOS_PATH,
         target_path=TARGET_PATH,
@@ -32,7 +41,10 @@ if USE_FFMPEG:
         duration=DURATION,
         quiet=True
     )
-else:
+
+@timeit("Building video with moviepy took ")
+def building_video_moviepy():
+    print("Started building video...")
     builder = VideoBuilderMoviePy()
 
     # Pull videos from folder and resize them
@@ -52,3 +64,8 @@ else:
 
     # Build the final video from the choosen videos
     builder.concat_videos(choosen_videos, "./results/result3.mp4")
+
+
+if __name__ == "__main__":
+    scrapping()
+    building_video_ffmpeg()
