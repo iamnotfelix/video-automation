cf9816a4809d8d506d6892fdbd7397c9b3846da4
iamnotfelix <andrei.iliescu03@gmail.com>
2023-07-22 22:01:40 +0300

    Added function that chooses random thumbnail.

--- thumbnail_scrapper.py
+++ thumbnail_scrapper.py
@@ -1,4 +1,5 @@
 import os
+import random
 from urllib import request
 import uuid
 from youtube import YoutubeController
@@ -7,10 +8,16 @@ from youtube import YoutubeController
 class ThumbnailScrapper:
     """ ThumbnailScrapper class provides an interface for scrapping thumbnails from youtube videos. """
 
-    def __init__(self) -> None:
+    def __init__(self, target_path: str = "thumbnails") -> None:
+        
+        # if target path does not exist, create it
+        if not os.path.exists(target_path) or not os.path.isdir(target_path):
+            os.mkdir(target_path)
+
+        self.target_path = target_path
         self.youtube = YoutubeController()
 
-    def get_thumbnails_from_channel(self, channel_id: str, target_path: str, nr: int = 0) -> None:
+    def get_thumbnails_from_channel(self, channel_id: str, nr: int = 0) -> None:
         video_ids = self.youtube.get_video_ids(channel_id)
 
         if nr > 0:
@@ -20,12 +27,12 @@ class ThumbnailScrapper:
             file_name = str(uuid.uuid4()) + ".jpeg"
             url = f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
             try:
-                request.urlretrieve(url, os.path.join(target_path, file_name))
+                request.urlretrieve(url, os.path.join(self.target_path, file_name))
             except Exception as e:
                 print(e)
                 print(video_id, url)
 
-    def get_thumbnail_from_video(self, video_url: str, target_path: str, file_name: str) -> None:
+    def get_thumbnail_from_video(self, video_url: str, file_name: str) -> None:
         if len(video_url) == 0:
             return
         
@@ -33,12 +40,18 @@ class ThumbnailScrapper:
         url = f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
 
         try:
-            request.urlretrieve(url, os.path.join(target_path, file_name))
+            request.urlretrieve(url, os.path.join(self.target_path, file_name))
         except Exception as e:
             print(e)
+
+    def choose_random_thumbnail(self) -> str:
+        thumbnails = [file_name for file_name in os.listdir(self.target_path)]
+        return os.path.join(self.target_path, random.choice(thumbnails))
 
 
 if __name__ == "__main__":
     thumbnail_scrapper = ThumbnailScrapper()
-    thumbnail_scrapper.get_thumbnail_from_video("https://www.youtube.com/watch?v=_GqP-qo0h7g", "./thumbnails", "asdf.jpeg")
-    thumbnail_scrapper.get_thumbnails_from_channel("UCJlxrVg_KbrVJIR3zoUlWxQ", "./thumbnails")
+    # thumbnail_scrapper.get_thumbnail_from_video("https://www.youtube.com/watch?v=_GqP-qo0h7g", "asdf.jpeg")
+    # thumbnail_scrapper.get_thumbnails_from_channel("UCJlxrVg_KbrVJIR3zoUlWxQ")
+    # print(thumbnail_scrapper.choose_random_thumbnail())
+
