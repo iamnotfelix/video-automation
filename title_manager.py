c4d74945f19cbcb6d7fdefb60f98d6ab8d2e9922
iamnotfelix <andrei.iliescu03@gmail.com>
2023-07-23 16:33:05 +0300

    Revamped title manager to use settings.

--- title_manager.py
+++ title_manager.py
@@ -1,5 +1,5 @@
-import json
 import random
+from settings import Settings
 
 from youtube import YoutubeController
 
@@ -7,27 +7,42 @@ from youtube import YoutubeController
 class TitleManager:
     """ TitleManager class provides a way to manage titles. """
 
-    def __init__(self, file_path: str = "titles.json") -> None:
-        self.file_path = file_path
-        self.youtube = YoutubeController()
+    def __init__(self, settings: Settings, youtube: YoutubeController) -> None:
+        self.settings = settings
+        self.youtube = youtube
 
-    def get_titles_from_channel(self, channel_id: str, nr: int = 0) -> None:
-        titles = self.youtube.get_video_titles(channel_id)
+    def delete_title(self, title: str) -> bool:
+        titles = self.settings.get_titles()
+        if title in titles:
+            titles.remove(title)
+            self.settings.set_titles(titles)
+            self.settings.save()
+            return True
+        
+        return False
+    
+    def add_titles(self, new_titles: list[str]) -> None:
+        titles = self.settings.get_titles()
+        for title in new_titles:
+            if not title in titles:
+                titles.append(title)
+        
+        self.settings.set_titles(titles)
+        self.settings.save()
 
-        if nr > 0:
-            titles = titles[:nr]
-
-        data = json.dumps({ "titles" : titles })
-        with open(self.file_path, "w", encoding="utf-8") as file:
-            file.write(data)
+    def get_all(self) -> list[str]:
+        self.settings.get_titles()
 
     def choose_random_title(self) -> str:
-        with open(self.file_path, "r") as file:
-            data = json.load(file)
-            return random.choice(data["titles"])
+        return random.choice(self.settings.get_titles())
 
 
 if __name__ == "__main__":
-    title_manager = TitleManager()
-    # print(title_manager.choose_random_title())
-    title_manager.get_titles_from_channel("UCJlxrVg_KbrVJIR3zoUlWxQ")
+    settings = Settings("test.json")
+    youtube = YoutubeController()
+    title_manager = TitleManager(settings=settings, youtube=youtube)
+    print(title_manager.choose_random_title())
+    # title_manager.add_titles(["asdf"])
+    # title_manager.add_titles(["asdf", "hjkl"])
+    # title_manager.add_titles(["to_delete"])
+    # title_manager.delete_title("to_delete")
